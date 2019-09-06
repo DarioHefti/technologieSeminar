27c3dae738e83a59646f684af999323804e7054b
DarioHefti <dario.hefti@bluewin.ch>
2019-09-06 17:36:06 +0200

    changes

--- src/main/java/com/abacus/technologieSeminar/repositories/UserRepository.java
+++ src/main/java/com/abacus/technologieSeminar/repositories/UserRepository.java
@@ -17,9 +17,11 @@ package com.abacus.technologieSeminar.repositories;
 import com.abacus.technologieSeminar.beans.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 public interface UserRepository extends JpaRepository<User,Long> {
 
   List<User> findBySurname(@Param("surname") String surname);
